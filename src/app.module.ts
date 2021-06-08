ec25fa7aa5f6ab64f3d98d3f2c35f1cb405a3815
Jim NTARE <jntare@bk.rw>
2021-06-08 12:52:19 +0200

    setting environment configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,22 +2,38 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksService } from './tasks/tasks.service';
-import { TasksRepository } from './tasks/tasks.repository';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
 
 @Module({
   imports: [
-    TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'ntare',
-      database: 'task-management',
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.NODE_ENV}`],
+      validationSchema: configValidationSchema,
     }),
+    TasksModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+        };
+      },
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    // }),
     AuthModule,
   ],
   providers: [TasksService],
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { TransformInterceptor } from './transform.interceptor';
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalInterceptors(new TransformInterceptor());
   const port = 3000;
   await app.listen(port);
